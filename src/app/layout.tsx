7c793274f9f6a7fcd83a449425fa862106e0b238
muhammadzhuhry <muhammadzhuhry@gmail.com>
2024-04-28 20:53:42 +0700

    Add LayoutContainer component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CProviders } from "./cproviders";
+import LayoutContainer from "./layout-container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <CProviders>
-          {children}
+          <LayoutContainer>
+            {children}
+          </LayoutContainer>
         </CProviders>
       </body>
     </html>
