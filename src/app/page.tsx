b5a9424b129878675680e795a74000136b3623cc
muhammadzhuhry <muhammadzhuhry@gmail.com>
2024-04-28 20:54:04 +0700

    Refactor page component to use PageTitle and Posts components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
-'use client'
-
-import { Avatar, Wrap, WrapItem } from '@chakra-ui/react'
+import PageTitle from "@/components/PageTitle";
+import Posts from "@/features/post/Posts";
+export const revalidate = 10;
 
 export default function Home() {
   return (
     <>
-      <Wrap>
-        <WrapItem>
-          <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Kola Tioluwani' src='https://bit.ly/tioluwani-kolawole' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
-        </WrapItem>
-        <WrapItem>
-          <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
-        </WrapItem>
-      </Wrap>
+      <PageTitle title="Posts" subtitle="Data posts" />
+      <Posts />
     </>
   )
 }
